66a71006b7b9020a5903baf11292fcd7451a420e
connorAlex <alex.connor.cochrane@gmail.com>
2022-10-13 12:17:02 -0500

    set up early dynamic card creation

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,15 @@
 import React, { useState, } from 'react'
 
 const Card = (props) => {
+  const {image, label} = props;
 
   return (
     <div className='card'>
-
+      <div>
+        Image
+        <img src={image} alt=''></img>
+      </div>
+      <div>{label}</div>
     </div>
   )
 }
--- src/components/main.js
+++ src/components/main.js
@@ -1,11 +1,14 @@
 import React, {useState} from 'react'
+import Card from './Card.js';
 
 const Main = () => {
-    const [cards, setCards] = useState([]);
+    const [cards, setCards] = useState([1]);
 
   return (
     <div className='Main'>
-
+        {cards.map(item => {
+            return <Card label="test"/>
+        })}
     </div>
   )
 }
